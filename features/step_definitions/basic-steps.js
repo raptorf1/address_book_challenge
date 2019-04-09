cccfeb54b7b19cfad2303efc1ce35103ffe51d35
stefan_karlberg <stefan.karlberg@smackit.se>
2019-04-09 15:46:22 +0200

    Added feature tests input buttons/form input. And implemented it´s code in index.htm

--- features/step_definitions/basic-steps.js
+++ features/step_definitions/basic-steps.js
@@ -14,13 +14,11 @@ Then('I should see {string}', async function(content) {
 })
 
 When('I click {string}', async function(string) {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending'
+  return await this.clickOnButton(string)
 })
 
-Then('I fill in {string} with {string}', async function(string, string2) {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending'
+Then('I fill in {string} with {string}', async function(field, content) {
+  return await this.fillFormField(field.toLowerCase(), content)
 })
 
 Then('I should have {int} contact in my address book', async function(int) {
