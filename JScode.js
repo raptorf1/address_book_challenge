7b98933c48d14ada0239b77c9b475a5f5220e64f
raptorf1 <raptor_f1@hotmail.com>
2019-04-12 22:32:23 +0200

    Adds 'Top' button when user scrolls down which takes him to the top of the webpage. Also when pressing 'edit' to a contact, the page scrolls up for the form to be visible.

--- JScode.js
+++ JScode.js
@@ -85,7 +85,23 @@ function editContact(id) {
         bRem.item(i).style.display = "none";
         }
 
-      document.getElementById("delete-contact").style = "display: none";     
+      document.getElementById("delete-contact").style = "display: none";
+      topFunction()
       }
     }  
 };
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
