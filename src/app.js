5abb28f807a839b5f27a2476de67dd02ec54fb4d
stefan_karlberg <stefan.karlberg@smackit.se>
2019-04-14 15:35:33 +0200

    Added new card code connected to new stylesheet.

--- src/app.js
+++ src/app.js
@@ -19,28 +19,35 @@ const renderContacts = () => {
         let li = document.createElement('li')
         li.innerHTML = `
  
-               <div class="flex mb-4" id:"card">
-                   <div class="w-2/5 image1 p-3">
-                      <img src = "http://www.stefankarlberg.se//bilder_craft/person.jpg" class = "image_person">
-                   </div>
-                   <div class="w-3/5 p-3 pr-4">
-                      <h2>${ contact.name }</h2>
-                      <h4>${ contact.company }</h4>
-                      <h5>${ contact.phone }</h5>
-                      <h6><a href="mailto:${ contact.email }">${ contact.email }</a></h6>
-                      <p>${ contact.notes }</p> 
-                        <span style="font-size: 1.5em; color: black;">
-                          <a href="https://www.twitter.com/${ contact.twitter}" target="_blank">
-                          <i class="fab fa-twitter"></i>
-                          </a>
-                        </span
-                    </div>
-               
-                    <div class = "btn_delete23">
-                      <button id = "button_card_delete" style="display:none" class="button_remove bg-red hover:bg-red-dark text-white py-1 px-2" onclick="deleteContact(${contact.id})">Delete</button>      
-                      <button id = "button_card_edit" style="display:none" class="button_edit bg-green hover:bg-green-dark text-white py-1 px-2" onclick="editContact(${contact.id})">Edit</button>      
-                    </div>
-              </div  
+      <div class ="card w-full sm:flex ">
+        <div class = "card_column_left flex-inline">
+            <div class = "card_column_left_image flex-none">
+            </div>
+        </div>
+       <div class="p-4 pl-6 flex flex-col">
+           <div>
+              <h2>${ contact.name }</h2>
+              <h3>${ contact.company }</h3>
+              <h4>${ contact.phone }</h4>
+              <p>${ contact.notes }</p>
+           </div>
+
+          <div class="flex card_bottom pin-b pin-r card_column_bottom">
+              <div class="w-2/5 float:left">
+                  <span style="font-size: 2em; color: black; margin-right:10px;">
+                  <a href="mailto:${ contact.email }"><i class="fas fa-envelope fa-w-16"></i></a>
+                  </span>
+                  <span style="font-size: 2em; color: black;">
+                  <a href="https://www.twitter.com/${ contact.twitter}"><i class="fab fa-twitter"></i></a>
+                  </span>
+              </div>
+              <div class="w-3/5 style = "float:right">
+                  <button id = "button_card_delete" style="display:none" class="button_remove card_button bg-red hover:bg-red-dark text-white py-2 px-3" onclick="deleteContact(${contact.id})">Delete</button>      
+                  <button id = "button_card_edit" style="display:none" class="button_edit card_button bg-green hover:bg-green-dark text-white py-2 px-3" onclick="editContact(${contact.id})">Edit</button>      
+              </div>
+          </div>
+        </div>
+    </div>
       
       `
         // Add the contact's li to the unordered list we created earlier
